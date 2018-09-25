319d25cae2526c3a5c3ac8e8a133bd8924ddf5e4
José María Domínguez Moreno <miso.0b11@gmail.com>
2018-09-24 22:26:53 -0400

    Import current_org and current_user into the root module
    This make ``current_user`` and ``current_org`` available in the package
    saraki so we can avoid importing them from saraki.auth module.

--- saraki/__init__.py
+++ saraki/__init__.py
@@ -8,4 +8,4 @@ __version__ = "0.1.0a0"
 
 
 from saraki.app import Saraki, Blueprint  # noqa: F401
-from saraki.auth import require_auth  # noqa: F401
+from saraki.auth import current_org, current_user, require_auth  # noqa: F401
